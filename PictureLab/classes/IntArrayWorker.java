cce61c5042cf24efe42f5ecba5ece3ee3dca048b
maalishah <ahad.shah25@gmail.com>
2015-01-06 12:21:07 -0600

    matrix
    method writing

--- PictureLab/classes/IntArrayWorker.java
+++ PictureLab/classes/IntArrayWorker.java
@@ -98,5 +98,48 @@ public class IntArrayWorker
       }
     }
   }
- 
+  
+  public int getCount(int num)
+  {
+      int count = 0;
+      for (int row = 0; row < matrix.length; row++)
+      {
+          for (int col = 0; col < matrix[0].length; col++)
+          {
+              if ( matrix[row][col] == num)
+              {
+                  count++;
+              }
+          }
+      }
+      return count;
+  }
+  
+  public int getLargest()
+  {
+      int num = 0;
+      for (int row = 0; row < matrix.length; row++)
+      {
+          for (int col = 0; col < matrix[0].length; col++)
+          {
+              if (matrix[row][col] > num)
+              {
+                  num = matrix[row][col];
+              }
+          }
+      }
+      return num;
+  }
+  
+  public int getColTotal(int num)
+  {
+      
+      for (int row = 0; row < matrix.length; row++)
+      {
+          for (int col = 0; col < matrix[0].length; col++)
+          {
+              
+          }
+      }
+  }
 }
